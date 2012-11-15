45b5c748beccecce7262b16b2ef660c2e7c36bcc
nabuk <color.ui>
2012-11-15 18:45:21 +0100

    faster build; improved ticker file colors

--- BuildRelease.bat
+++ BuildRelease.bat
@@ -1,3 +1,3 @@
 @echo off
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild /nologo /verbosity:minimal BuildRelease.msbuild
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild /nologo /verbosity:minimal /m:8 BuildRelease.msbuild
 pause

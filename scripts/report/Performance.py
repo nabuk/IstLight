9bcf9cebfb951e568bf46e675a7e5e8caadd3d0c
nabuk <color.ui>
2012-11-24 16:52:17 +0100

    added report (a.k.a. summary) section

--- scripts/report/Performance.py
+++ scripts/report/Performance.py
@@ -1,4 +1,7 @@
+from System import *
+from System.Collections.Generic import *
+
 Category = 'Performance'
 
 def Analyze(result):
-	return Array[KeyValuePair[string,string]]((KeyValuePair[string,string]('key','value'),))
+	return Array[KeyValuePair[String,String]]([KeyValuePair[String,String]('Summary','100'),KeyValuePair[String,String]('Summary2','200')])

0f37c0a70e82eb6e4631d8a78fcc91e516c711df
nabuk <color.ui>
2012-11-25 17:48:27 +0100

    more info about transactions is present now in equity tab

--- src/IstLight.ViewModel/ViewModels/ResultSections/EquityRowViewModel.cs
+++ src/IstLight.ViewModel/ViewModels/ResultSections/EquityRowViewModel.cs
@@ -17,13 +17,9 @@ namespace IstLight.ViewModels.ResultSections
             this.Cash = resultQuote.Cash;
             this.Total = resultQuote.Equity(result.SyncTickers);
             this.Portfolio = Total - Cash;
-            this.Commissions =
-                resultQuote.Transactions.Select(t =>
-                    result.Settings.Get<CommissionSetting>().ComputeFee(
-                        t.Quantity,
-                        result.SyncTickers.SimplifiedTickers[t.TickerIndex][
-                            result.SyncTickers[barIndex].CurrentQuoteCount[t.TickerIndex] - 1].Value))
-                .Sum();
+            this.Commissions = resultQuote.Transactions.Select(t => t.Commission).Sum();
+            this.TransactionCount = resultQuote.Transactions.Count;
+            this.TransactionTotal = resultQuote.Transactions.Select(t => t.Total).Sum();
 
             this.Bar = barIndex + 1;
         }
@@ -33,6 +29,8 @@ namespace IstLight.ViewModels.ResultSections
         public double Cash { get; private set; }
         public double Portfolio { get; private set; }
         public double Commissions { get; private set; }
+        public int TransactionCount { get; private set; }
+        public double TransactionTotal { get; private set; }
         public double Total { get; private set; }
     }
 }

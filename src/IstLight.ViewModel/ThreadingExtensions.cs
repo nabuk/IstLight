5f9755565264e0c0bde8760dabd1c86ab8274386
nabuk <color.ui>
2012-12-04 18:38:33 +0100

    extension refactor

---
+++ src/IstLight.ViewModel/ThreadingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace IstLight
+{
+    public static class ThreadingExtensions
+    {
+        public static void InvokeIfRequired(this Dispatcher dispatcher, Action action, DispatcherPriority priority = DispatcherPriority.DataBind)
+        {
+            if (dispatcher.Thread != Thread.CurrentThread)
+                dispatcher.Invoke(priority, action);
+            else
+                action();
+        }
+    }
+}

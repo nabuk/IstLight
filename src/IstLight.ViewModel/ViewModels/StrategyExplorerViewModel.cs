297a213a2ca8ef8dd4e761a6670066b60b59ab9e
nabuk <color.ui>
2012-12-04 20:10:10 +0100

    copyright headers

--- src/IstLight.ViewModel/ViewModels/StrategyExplorerViewModel.cs
+++ src/IstLight.ViewModel/ViewModels/StrategyExplorerViewModel.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections.ObjectModel;
+﻿// Copyright 2012 Jakub Niemyjski
+//
+// This file is part of IstLight.
+//
+// IstLight is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// IstLight is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with IstLight.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 
 namespace IstLight.ViewModels

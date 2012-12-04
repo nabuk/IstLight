d3c949d43a3cbe4da5ae0091c02d447e16ca9cef
nabuk <color.ui>
2012-12-04 10:42:54 +0100

    Added shared global assembly info

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+[assembly: AssemblyProduct("IstLight")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyTrademark("")]
+
+#if DEBUG
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
